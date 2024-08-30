b9e595f09561fe7dfda36cc49b176c4a35e76a95
Joao Gabriel Borges Santos <joao.santos@RPA.REDE>
2024-08-30 09:29:34 -0300

    minor changes

--- gguanabara/testing.py
+++ gguanabara/testing.py
@@ -22,37 +22,36 @@ else:
     else:
         print('unbalanced')
 
-# # My Response:
 
-# obj = input('Type an expression with parentheses: ')
-# par = ('(', ')')
+obj = input('Type an expression with parentheses: ')
+par = ('(', ')')
 
 
-# if obj[0] == par[1] or obj[-1] == par[0]:
-#     print('unbalanced')
+if obj[0] == par[1] or obj[-1] == par[0]:
+    print('unbalanced')
 
-# first_list = list()
-# for i in range(len(obj)):
-#     if obj[i] in par:
-#         first_list.append(obj[i])
+first_list = list()
+for i in range(len(obj)):
+    if obj[i] in par:
+        first_list.append(obj[i])
 
-# # balanced = False
-# opening_parentheses = list()
-# for i in first_list:
-#     if i == par[1] and not opening_parentheses:
-#         balanced = False
-#     elif i == par[0]:
-#         opening_parentheses.append(i)
-#     elif i == par[1]:
-#         opening_parentheses.pop()
+# balanced = False
+opening_parentheses = list()
+for i in first_list:
+    if i == par[1] and not opening_parentheses:
+        balanced = False
+    elif i == par[0]:
+        opening_parentheses.append(i)
+    elif i == par[1]:
+        opening_parentheses.pop()
 
-# if not opening_parentheses:
-#     balanced1 = True
-# else:
-#     balanced1 = False
+if not opening_parentheses:
+    balanced1 = True
+else:
+    balanced1 = False
 
-# if balanced == False and balanced1 == False:
-#     print('not balanced')
-# else:
-#     print('balanced')
+if balanced == False and balanced1 == False:
+    print('not balanced')
+else:
+    print('balanced')
 
